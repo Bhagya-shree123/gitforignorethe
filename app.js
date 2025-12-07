d549d2cf970595f8eca3d891893aeab8c04ce6e5
Bhagya-shree123 <b8958742@gmail.com>
2025-12-07 12:27:15 +0530

    Initial Node App

---
+++ app.js
@@ -0,0 +1,8 @@
+const express =reuire('express');
+const app =express();
+
+app.get('/',(req, res)=> {
+    res.send('Welcome to Devops App using Docker and Jenkins!');
+});
+
+app.listen(3000,() => console.log('App is running at 3000 '));
